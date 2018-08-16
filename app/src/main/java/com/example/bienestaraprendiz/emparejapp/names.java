a85b84211d6881d59de572ec055151e81f51cb94
Alirio <alirioobregon15@gmail.com>
2018-08-16 16:03:01 -0500

    agrego

---
+++ app/src/main/java/com/example/bienestaraprendiz/emparejapp/names.java
@@ -0,0 +1,13 @@
+package com.example.bienestaraprendiz.emparejapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class names extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_names);
+    }
+}
